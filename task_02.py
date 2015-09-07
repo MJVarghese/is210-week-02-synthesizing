016882ff508408a03bf991bb624d80d2906005f2
mjvarghese <mjvarghese@optonline.net>
2015-09-07 19:17:46 +0000

    Completing week 2 assignment

--- task_02.py
+++ task_02.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Task number 2"""
 
 print "Hello Student!"
--- task_03.py
+++ task_03.py
@@ -1,7 +1,12 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task number 3"""
+
+
+CHARLIE = "Brown"
+
 VIOLET = "Gray"
+
 PATRICIA = "Reichardt"
+
 LINUS = "van Pelt"
-
-
-
